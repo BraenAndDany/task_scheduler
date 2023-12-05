01c79d9cc491e5a1c08ed72b577bf80797e21ab2
Max <Xetyq@yandex.ru>
2023-12-05 14:28:13 +0700

    Добавьте файлы проекта.

---
+++ task_scheduler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+
+string dir = "C:\\Users\\User\\Desktop\\task scheduler";
+int i = 0;
+DirectoryInfo files = new DirectoryInfo(dir);
+FileInfo[] fls;
+fls = files.GetFiles();
+foreach (FileInfo file in fls)
+{
+    i++;
+}
+if (!Directory.Exists(dir)) 
+    Directory.CreateDirectory(dir);
+File.WriteAllText(Path.Combine(dir, i+".txt"), Convert.ToString(DateTime.Now));
